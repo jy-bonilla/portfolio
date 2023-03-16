2199eae7d34742a8dceb615aeaf8514617ab1482
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-16 19:42:06 -0400

    added component cont,intro,port,testimonials,topbar,works

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,30 @@
 import "./topbar.scss"
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Topbar() {
     return (
-        <div className="topbar">Topbar</div>
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo"> genius.</a>
+                    <div className="itemContainer">
+                        <PersonIcon className="icon" />
+                        <span> 540.398.1266</span>
+                    </div>
+                    <div className="itemContainer">
+                        <EmailIcon className="icon" />
+                        <span>jy.bonillagil@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line 1"></span>
+                        <span className="line 2"></span>
+                        <span className="line 3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
