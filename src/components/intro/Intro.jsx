ae543e89b4927e95a0d58ff2b67c4e2afa68602d
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-17 15:42:21 -0400

    added ityped and fixed intro image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,32 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            strings: ['Developer', 'Designer', 'Content Creater'],
+        });
+    }, []);
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/joseBonillaBlank.png" alt="smiling man" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello, I'm</h2>
+                    <h1>José Bonilla</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
 
         </div>
     )
