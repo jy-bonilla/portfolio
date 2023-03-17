630c9e63f36d59cf74229e73d1963b81068e78fd
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-16 20:00:39 -0400

    added components: contact, intro, portfolio, testimonials, topbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import "./app.scss"
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(true)
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
