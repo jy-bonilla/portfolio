b7e27c76e4fa98d3224d37c27a49e9eef43013f9
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-18 23:23:30 -0400

    added PortfolioList component & function

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured")
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web App",
+        },
+        {
+            id: "mobile",
+            title: "Mobile App",
+        },
+        {
+            id: "design",
+            title: "Design",
+        },
+        {
+            id: "content",
+            title: "Content",
+        },
+    ];
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Feature</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                {list.map((item) => (
+                    <PortfolioList
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
                 <div className="item">
-                    <img src="" alt="" />
+                    <img src="https://www.pexels.com/photo/landscape-photography-of-mountain-and-body-of-water-2387418/" alt="" />
                     <h3>Banking App</h3>
                 </div>
             </div>
