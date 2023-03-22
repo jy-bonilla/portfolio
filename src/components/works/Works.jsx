c6b6e8267bb358cb563687ebed0ac6558b9dbbf4
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-21 22:58:21 -0400

    added slider to works and scss

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,6 +3,29 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="mobile" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Non, tempora, dolorum
+                                    aliquid perferendis illo mollitia doloremque
+                                    minima nostrum aut odit ad,
+                                    a eveniet id! Dignissimos expedita temporibus quod
+                                    cupiditate quas.
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
