a2b538d4ab7d143bc0c27c61fbfadd1efd59ce8c
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-21 23:03:41 -0400

    added right img to slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -22,7 +22,9 @@ export default function Works() {
                                 <span>Projects</span>
                             </div>
                         </div>
-                        <div className="right"></div>
+                        <div className="right">
+                            <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
