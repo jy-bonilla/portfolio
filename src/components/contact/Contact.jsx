4c44fac2c1975c3b78969e63c27c62ba61b6f7be
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-25 22:00:44 -0400

    created build folder to deploy site on Hostinger

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
             <div className="right">
                 <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeHolder="Email" />
+                    <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
                     {message && <span>Thanks, I will reply ASAP</span>}
