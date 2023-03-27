d6aff21db1693df56fa67caeb3193c4d4674ba46
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-03-26 22:25:18 -0400

    added about section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,17 @@
+
+export default function about() {
+    return (
+        <div className="about" id="paragraph">
+            <p>Aspiring software engineer with 5 years of clinical data analysis. Recently,
+                I pivoted my career from the lab sciences to software engineering due to my
+                passion for software technologies, love of learning and problem-solving abilities.
+
+                Currently, I focus on creating websites which are accesible,
+                responsive and customer driven. The technologies which I'm currently
+                using to achieve these goals are HTML/CSS and JavaScript on the client side,
+                Node.js and Express on the server side along with the React framework.
+            </p>
+        </div>
+    )
+}
+
